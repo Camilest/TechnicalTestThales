c9e492a0f38f1b7ab8a0a83de315fba7da816178
Cristhian Camilo Pérez Estrada <c.camilosky1120@gmail.com>
2021-12-01 08:32:46 -0500

    Actualización test

--- CristhianPerezE/CristhianPerezE/Scripts/App/Employee.js
+++ CristhianPerezE/CristhianPerezE/Scripts/App/Employee.js
@@ -5,20 +5,33 @@
 function getEmployees() {
     const url = '/Employee/GetEmployees';
 
+
     $.ajax({
         url: url,
         data: {
         },
         success: function (result) {
             $.each(result.EmployeesResult, function (idx, data) {
-                //$("ol").append("<li>Appended item</li>");
-                $("ol").append(`<li> ${data.id} - ${data.employee_name} - ${data.employee_salary} - ${data.employee_age} - ${data.employee_anual_salary} </li>`);
-
-                //$("#EmployeeInfo").text(`${data.id} - ${data.employee_name} - ${data.employee_salary} - ${data.employee_age} - ${data.employee_anual_salary}`);
+                $('#tbody').append(`<tr id="R${++idx}">
+                  <td class="row-index text-center">
+                        <p>${data.id}</p></td>
+                  <td class="row-index text-center">
+                        <p>${data.employee_name}</p></td>
+                  <td class="row-index text-center">
+                        <p>${data.employee_age}</p></td>
+                   <td class="row-index text-center">
+                        <p>${data.employee_salary}</p></td>
+                   <td class="row-index text-center">
+                        <p>${data.employee_anual_salary}</p></td>
+                   </tr>`);
+                //$("ol").append(`<li> ${data.id} - ${data.employee_name} - ${data.employee_salary} - ${data.employee_age} - ${data.employee_anual_salary} </li>`);
             });
         },
         error: function (request, status, error) {
-            alert(status + ", " + error);
+            $("#error").append(`<div class="alert alert-warning" role="alert">
+                  Se ha generado un error.
+                </div>`);
+            //alert(status + ", " + error);
         }
     });
 }
@@ -36,12 +49,26 @@ function getEmployeeById() {
             id: idEmployee
         },
         success: function (result) {
-            console.log(result);
-            //$("#EmployeeInfo").text(`${result.id} - ${result.employee_name} - ${result.employee_salary} - ${result.employee_age} - ${result.employee_anual_salary}`);
-            $("ol").append(`<li> ${result.id} - ${result.employee_name} - ${result.employee_salary} - ${result.employee_age} - ${result.employee_anual_salary} </li>`);
+            $('#tbody').append(`<tr>
+                  <td class="row-index text-center">
+                        <p>${result.id}</p></td>
+                  <td class="row-index text-center">
+                        <p>${result.employee_name}</p></td>
+                  <td class="row-index text-center">
+                        <p>${result.employee_age}</p></td>
+                   <td class="row-index text-center">
+                        <p>${result.employee_salary}</p></td>
+                   <td class="row-index text-center">
+                        <p>${result.employee_anual_salary}</p></td>
+                   </tr>`);
+            //console.log(result);
+            //$("ol").append(`<li> ${result.id} - ${result.employee_name} - ${result.employee_salary} - ${result.employee_age} - ${result.employee_anual_salary} </li>`);
         },
         error: function (request, status, error) {
-            alert(status + ", " + error);
+            $("#error").append(`<div class="alert alert-warning" role="alert">
+                  Se ha generado un error.
+                </div>`);
+            //alert(status + ", " + error);
         }
     });
 }
